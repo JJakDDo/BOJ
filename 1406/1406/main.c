3d4e1e8bd4ceb612a0c40c3a200f0fd18c83544c
JJakDDo <kangdw124@gmail.com>
2018-01-10 14:25:21 +0900

    제출했을때 틀렸다고 나옴.

--- 1406/1406/main.c
+++ 1406/1406/main.c
@@ -4,78 +4,111 @@
 typedef struct node{
     char data;
     struct node* next;
+    struct node* prev;
 } Node;
 
 Node* head = NULL;
 Node* cursor = NULL;
+Node* prev = NULL;
 
-Node* addNode(Node* prev, char ch){
+Node* addNode(char ch){
     Node* new = (Node*)malloc(sizeof(Node));
     new->data = ch;
+    //문자열이 비었을때 문자 추가
     if(head == NULL){
         head = new;
         new->next = NULL;
-        cursor = head;
+        new->prev = NULL;
         return head;
     }
     else{
-        new->next = prev->next;
-        prev->next = new;
-        cursor = new;
-        return new;
+        //커서가 맨 앞쪽일때 문자 추가
+        if(cursor == NULL){
+            new->prev = NULL;
+            new->next = head;
+            if(new->next != NULL)
+                new->next->prev = new;
+            head = new;
+            return new;
+        }
+        else{
+            new->next = cursor->next;
+            cursor->next = new;
+            new->prev = cursor;
+            if(new->next != NULL){
+                new->next->prev = new;
+            }
+            return new;
+        }
     }
     
 }
 
 void moveLeft(){
-    if(cursor == head)
+    if(cursor == NULL)
         return;
-    Node* temp = (Node*)malloc(sizeof(Node));
-    temp = head;
-    while(temp->next != cursor){
-        temp = temp->next;
-    }
-    cursor = temp;
+    cursor = cursor->prev;
 }
 void moveRight(){
+    if(cursor == NULL){
+        cursor = head;
+        return;
+    }
     if(cursor->next == NULL)
         return;
     cursor = cursor->next;
 }
-//다시풀기
+
 void deleteChar(){
-    Node* temp = (Node*)malloc(sizeof(Node));
-    Node* temp2 = (Node*)malloc(sizeof(Node));
-    temp = head;
-    while(temp->next != cursor)
-        temp = temp->next;
+    if(cursor == NULL)
+        return;
     
-    temp2 = temp->next;
-    temp->next = temp2->next;
-    cursor = temp;
-    free(temp2);
+    Node* temp = (Node*)malloc(sizeof(Node));
+    
+    temp = cursor;
+    //맨 앞 문자 지울때
+    if(temp->prev == NULL){
+        head = temp->next;
+        if(temp->next != NULL)
+            temp->next->prev = NULL;
+        cursor = temp->prev;
+    }
+    //맨 뒤 문자 지울때
+    else if(temp->next == NULL){
+        //문자열에 문자가 하나있을때 지우는지 확인
+        if(temp->prev != NULL){
+            temp->prev->next = NULL;
+            cursor = temp->prev;
+        }
+        else{
+            head = NULL;
+            cursor = head;
+        }
+    }
+    else{
+        temp->prev->next = temp->next;
+        temp->next->prev = temp->prev;
+        cursor = temp->next;
+    }
+    free(temp);
 }
 
 int main(int argc, const char * argv[]) {
     
-    char ch;
     char input[5];
     char *command;
-    Node* prev = NULL;
     int test;
-    while((ch=getc(stdin)) != '\n'){
-        prev = addNode(prev, ch);
-    }
-    
+    char line[100001];
+    scanf("%s", line);
+    int k = 0;
+    //문자열을 하나씩 읽어서 리스트에 추가
+    while(line[k] != '\0')
+        cursor = addNode(line[k++]);
     scanf("%d", &test);
-    ch = getc(stdin);
     int j;
     for(int i=0;i<test;i++){
         j = 0;
-        while((ch=getc(stdin)) != '\n'){
-            input[j++] = ch;
-        }
-        input[j]='\0';
+        scanf(" %[^\n]", input);
         if(strcmp(input, "L") == 0){
             moveLeft();
         }
@@ -88,7 +121,7 @@ int main(int argc, const char * argv[]) {
         else {
             command = strtok(input, " ");
             command = strtok(NULL, " ");
-            addNode(cursor, command[0]);
+            cursor = addNode(command[0]);
         }
     }
     
