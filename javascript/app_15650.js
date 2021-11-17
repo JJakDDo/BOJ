00e0f5bef0ea213f1cce87eaaedd4076b3c74164
JJakDDo <34996487+JJakDDo@users.noreply.github.com>
2021-11-17 12:48:56 +0900

    15650번 N과 M(2)

--- javascript/app_15650.js
+++ javascript/app_15650.js
@@ -3,25 +3,26 @@ let input = fs.readFileSync('./input.txt').toString().replace(/\r\n/g,'\n').spli
 let n = +input[0].split(" ")[0];
 let m = +input[0].split(" ")[1];
 
-let visited = Array(n + 1).fill(false);
+//let visited = Array(n + 1).fill(false);
 let output = [];
 let answer = "";
 
 function solution(num){
-    if(num == m){
+    if(output.length == m){
         answer += `${output.join(" ")}\n`;
+        return;
     }
 
-    for(let i = 1; i <= n; i++){
-        if(visited[i] || i < output[output.length-1]){
-            continue;
-        }
-        visited[i] = true;
+    for(let i = num; i <= n; i++){
+        //if(visited[i] || i < output[output.length-1]){
+        //    continue;
+        //}
+        //visited[i] = true;
         output.push(i);
-        solution(num+1);
+        solution(i+1);
         output.pop();
-        visited[i] = false;
+        //visited[i] = false;
     }
 }
-solution(0);
+solution(1);
 console.log(answer);
