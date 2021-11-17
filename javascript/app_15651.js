77a257b048b3c58e85f955751f500b34f12c1b6e
JJakDDo <34996487+JJakDDo@users.noreply.github.com>
2021-11-17 12:53:23 +0900

    15651번 N과 M(3)

---
+++ javascript/app_15651.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+let input = fs.readFileSync('./input.txt').toString().replace(/\r\n/g,'\n').split('\n');
+let n = +input[0].split(" ")[0];
+let m = +input[0].split(" ")[1];
+
+let output = [];
+let answer = "";
+
+function solution(num){
+    if(output.length == m){
+        answer += `${output.join(" ")}\n`;
+        return;
+    }
+    for(let i = num; i <= n; i++){
+        output.push(i);
+        solution(num);
+        output.pop();
+    }
+}
+solution(1);
+console.log(answer);
