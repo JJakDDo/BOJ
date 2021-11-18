30691ae01fa14abe8b0110c715a7892adfc0f154
JJakDDo <34996487+JJakDDo@users.noreply.github.com>
2021-11-18 19:44:15 +0900

    15666번 N과 M(12)

---
+++ javascript/app_15666.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+let input = fs.readFileSync('./input.txt').toString().replace(/\r\n/g,'\n').split('\n');
+let n = +input[0].split(" ")[0];
+let m = +input[0].split(" ")[1];
+let inputNum = input[1].split(" ").map(e => +e).sort((a,b) => a-b);
+
+let output = [];
+let answer = [];
+
+function solution(num){
+    if(output.length == m){
+        answer.push([...output]);
+        return;
+    }
+    for(let i = num; i < n; i++){
+        output.push(inputNum[i]);
+        solution(i);
+        output.pop();
+    }
+}
+solution(0);
+const set = new Set(answer.map(e => e.join(" ")));
+console.log([...set]. join("\n"));
