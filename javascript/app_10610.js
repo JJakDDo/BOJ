ffebd1cde2d77e4c96c6eeeb567ca4d1493a8cff
JJakDDo <34996487+JJakDDo@users.noreply.github.com>
2021-12-17 00:13:00 +0900

    Update app_10610.js
    이제는 메모리 초과뜸

--- javascript/app_10610.js
+++ javascript/app_10610.js
@@ -1,29 +1,38 @@
 const fs = require('fs');
 let input = fs.readFileSync('./input.txt').toString().replace(/\r\n/g,'\n').split('\n');
-let number = input.shift().split("").map(e => +e).sort((a,b) => b-a);
-let output = [];
-let visitied = Array(number.length).fill(false);
+let num = input.shift().split("").map(e => +e).sort((a,b) => b-a);
 let answer = -1;
+let n = num.length;
+let index;
+if(num[n-1] == 0){
+    do{
+      if(num.join("")%30 == 0){
+          answer = num.join("");
+          break;
+      }
+      index = -1;
+      for(let i = n-3; i >= 0; i--){ 
+        if(num[i] > num[i+1]){ 
+          index = i;
+          break;
+        }
+      }
+      if(index < 0){
+          break; 
+      } else {
+        for(let i = n-2; i > index; i--){
+          if(num[i] < num[index]){  
+            const temp = num[i]; 
+            num[i] = num[index];
+            num[index] = temp;
+            break;
+          }
+        }
+        num = [...num.slice(0, index+1), ...num.slice(index+1, n).reverse(), 0]; 
+      } 
+    } while(index >= 0)
 
-function solution(count){
-    if(count == number.length){
-        if(output.join("")%30 == 0){
-            answer = output.join("");
-        }
-        return;
-    }
-    for(let i = 0; i < number.length; i++){
-        if(!visitied[i]){
-            visitied[i] = true;
-            output.push(number[i]);
-            solution(count+1);
-            output.pop();
-            visitied[i] = false;
-        }
-        if(answer != -1){
-            return;
-        }
-    }
+    console.log(answer);
+} else {
+    console.log(-1);
 }
-solution(0);
-console.log(answer);
